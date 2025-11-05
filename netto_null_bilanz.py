13e346286ab423d07a9090bc453263c66c52b550
shlprt <simon.hilpert@posteo.de>
2025-11-05 11:14:30 +0100

    Allow for building green via point geometry

--- netto_null_bilanz.py
+++ netto_null_bilanz.py
@@ -3,12 +3,11 @@ from qgis.PyQt.QtCore import QCoreApplication, Qt
 from qgis.PyQt.QtWidgets import QAction, QMessageBox
 from qgis.PyQt.QtGui import QIcon
 from qgis.utils import iface
-from qgis.core import QgsProject
 import os
-import traceback
+import traceback 
 
 from .netto_null_bilanz_dialog import NettoNullBilanzDialog
-from . import script_core, plotting
+from . import script_core
 
 
 class NettoNullBilanz:
@@ -21,18 +20,6 @@ class NettoNullBilanz:
         """
         self.iface = iface
         self.plugin_dir = os.path.dirname(__file__)
-        project = QgsProject.instance()
-        project_path = project.fileName()
-
-        if project_path:
-            project_dir = os.path.dirname(project_path)
-            self.output_dir = os.path.join(project_dir, "bgib-results")
-        else:
-            # fallback if no project is saved yet
-            self.output_dir = os.path.expanduser("~/bgib-results")
-
-        os.makedirs(self.output_dir, exist_ok=True)
-
         self.action = None
         self.dlg = None
 
@@ -68,14 +55,16 @@ class NettoNullBilanz:
 
         # Retrieve parameters from dialog
         params = self.dlg.get_parameters()
+        print(params)
         base_layer_name = params["base_layer_name"]
         base_field_name = params["base_field_name"]
         plan_layer_name = params["plan_layer_name"]
         plan_field_name = params["plan_field_name"]
         factors_csv = params["factors_csv"]
-        output_csv_path = os.path.join(self.output_dir, "bgig_balance.csv")
+        output_csv_path = params["output_path"]
         building_green = params["building_green"]
-
+        building_green_layer_name = params["building_green_layer_name"]
+        
         # Validate required inputs
         if not base_layer_name or not base_field_name or not plan_layer_name or not plan_field_name:
             QMessageBox.warning(
@@ -85,37 +74,35 @@ class NettoNullBilanz:
             )
             return
 
-        #if not output_csv_path:
-        #    QMessageBox.warning(None, "Missing Output", "Please specify an output CSV file path.")
-        #    return
+        if not output_csv_path:
+            QMessageBox.warning(None, "Missing Output", "Please specify an output CSV file path.")
+            return
 
         try:
             # Call the processing function in script_core
-            results_info, df = script_core.main(
+            results = script_core.main(
                 base_layer_name=base_layer_name,
                 base_field_name=base_field_name,
                 planning_layer_name=plan_layer_name,
                 plan_field_name=plan_field_name,
                 factors_csv=factors_csv,
                 output_csv_path=output_csv_path,
-                building_green = building_green
+                building_green = building_green,
+                building_green_layer_name=building_green_layer_name
             )
-
-
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Information)
             msg.setWindowTitle("Success")
             msg.setTextFormat(Qt.PlainText)  # prevents rich text parsing (no unwanted wrapping)
             msg.setText("✅ Results exported successfully!")
-            text = "\n \n".join([f"{k}: {v}" for k, v in results_info.items()])
+            text = "\n \n".join([f"{k}: {v}" for k, v in results.items()])
             msg.setInformativeText(text)
             msg.exec_()
             
-            plotting.waterfall(df, self.output_dir)
-
             #QMessageBox.information(None, "Success", f"✅ Results exported successfully!\n\n{msg}")
-
+            
 
         except Exception as e:
-            traceback.print_exc()  # <-- Full traceback in the Python console
-            QMessageBox.critical(None, "Error", f"❌ {str(e)}")
+    	    traceback.print_exc()  # <-- Full traceback in the Python console
+    	    QMessageBox.critical(None, "Error", f"❌ {str(e)}")
+
